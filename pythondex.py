47736932e20b7b9371e19bb63109e5288307fc81
Rodolfo@WSL <rodo.aravena@outlook.com>
2019-10-11 17:44:10 -0300

    Limpiando codigo para enviar

--- pythondex.py
+++ pythondex.py
@@ -1,46 +1,6 @@
 from __future__ import print_function
-from PIL import Image
 from dex import get_pokemon_data, get_pokemon_learnset, get_move
-import numpy as np
-import sys, os
-
-def check_pokedex():
-    print('Testing integrity data, please wait...')
-    data = []
-    ok_data = []
-    count_error = 0
-    count_ok = 0
-    for pokemon in get_pokemon_data().keys():
-        learnset = get_pokemon_learnset(str(pokemon))
-        if learnset is not None:
-            for move in learnset:
-                try:
-                    ok_data.append(get_move(move)['name'])
-                    count_ok += 1
-                except:
-                    if move not in data:
-                        data.append(move)
-                        count_error+=1
-    if count_error > 0:
-        print(data)
-        print('We found {} errors.\nThese attack are not in dex: '.format(count_error))
-    else:
-        print('No error found')
-    print('Data ok ', count_ok)
-
-# Args
-argumentos = sys.argv
-if 'check' in argumentos:
-    check_pokedex()
-    exit()
-
-
-#Asscinator setup
-chars = np.asarray(list(' .,:;irsXA253hMHGS#9B&@'))
-f = ''
-SC = float(0.15)
-GCF = float(1) 
-WCF = 7.0/4.0
+import os, sys
 
 print ("Bienvenido")
 name_pokemon = ""
@@ -51,31 +11,56 @@ while(pokedex_on):
     name_pokemon = input("Ingrese el nombre del Pokémon: ")
     name_pokemon = name_pokemon.lower()
     os.system('clear')
-    pokemon = get_pokemon_data(name_pokemon)
-
-    if pokemon is not None:
-        # Integracion de asciinator para mostrar imagen ascii del pokemon
-        f = 'pokemon_images/'+ '{:04d}'.format(pokemon['num'])+'.png'
-        img_pokemon = Image.open(f)
-        S = (int(img_pokemon.size[0]*SC*WCF), int(img_pokemon.size[1]*SC))
-        img_pokemon = np.sum( np.asarray(img_pokemon.resize(S), dtype="float"), axis=2)
-        img_pokemon -= img_pokemon.min()
-        img_pokemon = (1.0 - img_pokemon/img_pokemon.max())**GCF*(chars.size-1)
-
-        print( "\n".join(("".join(r) for r in chars[img_pokemon.astype(int)])))
-        print()
-
-        # Muestra los datos principales del pokemon
-        print ("Numero del Pokémon: ", pokemon['num'])
-        print ("Sus tipos son: ")
-        for pokemon_type in pokemon["types"]:
+    pokemon = get_pokemon_data(name_pokemon) #E
+    '''
+    * La función 'get_pokemon_data' recibe un str y devuelve un diccionario con los datos de el Pokémon consultado
+    * Las llaves que contiene el diccionario son las siguiente:
+        {
+            "num" -> Número del Pokémon
+            "species"-> Nombre del Pokémon
+            "types" -> Lista que contiene el o los Tipos del Pokémon ejemplo: ["Grass", "Poison"],
+            "genderRatio" -> Contiene un diccionario con valores float que determina la probabilidad de aparición en la hierba
+                                ejemplo: { "M": 0.875, "F": 0.125 },
+            "gender": -> Contiene un valor str y solo indica que el Pokémon es de un sólo genero, Ejemplo: "N",
+                            esta llave puede tener los siguientes valores: N (Neutral), F (Femenino) y M (Masculino)
+            "baseStats" -> Esta llave contiene un diccionario con valores int y representan los stats base hp, ataque, defensa, ataque especial, defensa especial y velocidad
+                            Ejemplo: { "hp": 45, "atk": 49, "def": 49, "spa": 65, "spd": 65, "spe": 45 },
+            "abilities" -> Esta llave contiene un diccionario con las habilidades, Ejemplo: { "0": "Overgrow", "H": "Chlorophyll" },
+            "heightm" -> Esta llave contiene un valor float la cual muestra la altura del Pokémon en metros, Ejemplo: 0.7,
+            "weightkg" -> Esta llave contiene un valor float la cual muestra el peso en kilos del Pokémon, Ejemplo: 6.9,
+            "color" -> Esta llave contiene un valor str indicando el color primario del Pokémon, Ejemplo: "Green",
+            "evos" -> Contiene una lista de valores str que almacena las siguientes evoluciones del Pokémon, Ejemplo: ["ivysaur"],
+            "eggGroups" -> Contiene la el grupo de huevo del Pokémon y es una lista con valores str, Ejemplo ["Monster", "Grass"],
+            "otherFormes" -> Contiene una lista con valores str en su interior y muestra las formas regionales o mega evoluciones del Pokémon
+                            Ejemplo: ["exeggutoralola"] o ["latiosmega"]
+        }
+    * Nota: Algunas llaves pueden tanto existir como no existir en el diccionario devuelto, por lo que 
+            se recomineda verificar verificar si la llave existe en el diccionario
+    '''
+    if pokemon is not None: # Si lo que devuelve la funcion get_pokemon_data no es None entonces muestra la información
+        # Muestra los datos principales del Pókemon        
+        print ("\nPokémon número ", pokemon['num']) # Número del Pokémon
+        print ("\nNombre del Pokémon: ", pokemon['species']) #Nombre del Pokémon
+        print ("\nSus tipos son: ")
+        for pokemon_type in pokemon["types"]: # Se recorre la lista e imprime en pantalla los tipos
             print("  - ",pokemon_type)
-        if "genderRatio" in pokemon:
-            print("Posibilidad de aparición según género: ")
-            print("  - Macho = ", (pokemon["genderRatio"]["M"] * 100), "%")
-            print("  - Hembra = ",(pokemon["genderRatio"]["F"] * 100), "%")
-        elif "gender" in pokemon:
-            print("Género único:")
+
+        if "baseStats" in pokemon: # Se verifica si la llave existe
+            print("\nEstadísticas base del Pokémon: ")
+            print("  - HP = ", (pokemon["baseStats"]["hp"])) 
+            print("  - Ataque = ",(pokemon["baseStats"]["atk"])) 
+            print("  - Defensa = ", (pokemon["baseStats"]["def"])) 
+            print("  - Ataque especial = ",(pokemon["baseStats"]["spa"])) 
+            print("  - Defensa especial = ", (pokemon["baseStats"]["spd"])) 
+            print("  - Velocidad = ",(pokemon["baseStats"]["spe"])) 
+
+        if "genderRatio" in pokemon: # Se verifica si la llave existe
+            print("\nPosibilidad de aparición según género: ")
+            print("  - Macho = ", (pokemon["genderRatio"]["M"] * 100), "%") 
+            print("  - Hembra = ",(pokemon["genderRatio"]["F"] * 100), "%") 
+
+        elif "gender" in pokemon: # Si no exite se verifica la segunda posibilidad, de lo contrario no se muestra
+            print("\nGénero único:")
             if pokemon["gender"] == "N":
                 print("  - Neutral")
 
@@ -85,26 +70,24 @@ while(pokedex_on):
             elif pokemon["gender"] == "M":
                 print("  - Macho")
         
-        if "evos" in pokemon:
-            print("Siguiente evolución: ")
-            for evo in pokemon["evos"]:
+        if "evos" in pokemon: # Se verifica si el Pokémon tiene evoluciones
+            print("\nSiguiente evolución: ")
+            for evo in pokemon["evos"]: # Si tiene evoluciones se recorre la lista y se imprime su nombre
                 pokemon_evo = get_pokemon_data(evo)
                 print("  - ", pokemon_evo["species"])
 
-               
-
-        if "otherFormes" in pokemon:
-            print("Otras formas del Pokémon: ")
-            for other_form in pokemon["otherFormes"]:
+        if "otherFormes" in pokemon: # Se verifica si el Pokémon posee otra forma regional y/o Mega evolución
+            print("\nOtras formas del Pokémon: ")
+            for other_form in pokemon["otherFormes"]:# Si es verdadero se consulta la información
                 form_pokemon = get_pokemon_data(other_form)                
                 new_name_pokemon = str(form_pokemon["species"]).split('-')
                 print ("  - ", new_name_pokemon[1], new_name_pokemon[0])
-        print('Movimientos que puede aprender el pokémon: ')
-        for move in get_pokemon_learnset(name_pokemon):
 
+        print('\nMovimientos que puede aprender el pokémon: ')
+        for move in get_pokemon_learnset(name_pokemon):
             print('  - ', str(get_move(move)['name']))
-    else:
-        print("Pokemon no encontrado :(")
+    else:# En caso de que no se encuentre el Pokémon muestra un muestra un mensaje de no encontrado
+        print("Pokemon no encontrado")
 
     
 
